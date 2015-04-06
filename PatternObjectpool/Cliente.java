d840522c841278653ef65da54093487f9a4a6124
BLACKJALS <jholeon@uan.edu.co>
2015-04-05 22:13:49 -0500

    Patrones

---
+++ PatternObjectpool/Cliente.java
@@ -0,0 +1,13 @@
+
+package PatternObjectpool;
+
+import java.util.Scanner;
+
+public abstract class Cliente {
+
+    public static void main (String args[]){        
+      Reusable reusable;
+      reusable = new Reusable();  
+   
+  }
+}
